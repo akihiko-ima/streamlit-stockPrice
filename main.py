4ceb4da5f2a5c02b9da3d81bae2b493e29fa8651
akihiko-ima <sr.cb.ape@gmail.com>
2024-05-22 00:28:42 +0900

    refactor: 見た目改善

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Streamlit 株価表示アプリ
+# Streamlit 株価表示アプリ
 
 import pandas as pd
 import yfinance as yf
@@ -8,9 +8,10 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.io as pio
 
-st.title("株価可視化アプリ")
+st.title("StockPrice-Viewer")
 
-#キャッシュを貯めるコマンド。次回以降の検索速度が工場
+
+# キャッシュを貯めるコマンド。次回以降の検索速度が工場
 @st.cache_data
 def get_data(days, tickers):
     df = pd.DataFrame()
@@ -25,14 +26,17 @@ def get_data(days, tickers):
         df = pd.concat([df, hist])
     return df
 
-#tryを使用することで、エラー発生時のコメント表示が可能
-try:
-    #st.write("#### ■表示日数選択")
-    days = st.sidebar.slider("日数", 1, 2000, 500)
-    #st.write(f"#### 過去 **{days}日間**の株価")
 
-    st.write("#### 会社選択")
-    ymin, ymax = st.sidebar.slider("範囲", 0, 3000, (0, 500))
+# tryを使用することで、エラー発生時のコメント表示が可能
+try:
+    # st.write("#### ■表示日数選択")
+    days = st.sidebar.slider(":calendar: Days", 1, 2000, 500)
+    # st.write(f"#### 過去 **{days}日間**の株価")
+
+    st.write("#### Company Selection")
+    ymin, ymax = st.sidebar.slider(
+        ":chart_with_upwards_trend: Scale ", 0, 3000, (0, 500)
+    )
 
     tickers = {
         "KO": "KO",
@@ -43,44 +47,60 @@ try:
         "ETF VT": "VT",
     }
 
-    df =get_data(days, tickers)
+    df = get_data(days, tickers)
 
     companies = st.multiselect(
-        "会社選択", list(df.index), ["KO", "Microsoft", "ETF VTI", "ETF VT"]#ko, MSFT, VTI, VTを初期表示
+        "Company Selection",
+        list(df.index),
+        ["KO", "Microsoft", "ETF VTI", "ETF VT"],  # ko, MSFT, VTI, VTを初期表示
     )
     if not companies:
         st.error("一社は選択する")
     else:
         data = df.loc[companies]
-        st.write("#### 株価(USD)", data.sort_index())
-        #yfinanceから読み取ったデータフレームのインデックスがずれているのでreset実施
+        st.write("#### StockPrice(USD)", data.sort_index())
+        # yfinanceから読み取ったデータフレームのインデックスがずれているのでreset実施
         data = data.T.reset_index()
-        data =pd.melt(data, id_vars=["Date"]).rename(
-            columns={"value":"Stock Prices(USD)"}
+        data = pd.melt(data, id_vars=["Date"]).rename(
+            columns={"value": "Stock Prices(USD)"}
         )
         df = df.T.reset_index()
         fig1 = make_subplots(rows=1, cols=1)
 
     # for company in tickers.keys():
     for company in companies:
-        fig1.add_trace(go.Scatter(x=df["Date"], y=df[company], name=f"{company}"), row=1, col=1)
-        fig1.update_yaxes(title_text="株価 [$]", title_font = {"size": 20}, title_standoff = 0,  range=[ymin, ymax], row=1, col=1)
+        fig1.add_trace(
+            go.Scatter(x=df["Date"], y=df[company], name=f"{company}"), row=1, col=1
+        )
+        fig1.update_yaxes(
+            title_text="StockPrice [$]",
+            title_font={"size": 20},
+            title_standoff=0,
+            range=[ymin, ymax],
+            row=1,
+            col=1,
+        )
 
         fig1.update_layout(height=700, width=950)
-        fig1.update_layout(title=dict(text='<b>株価',
-                                    font=dict(size=20, color="grey"),
-                                    x=0.45, xanchor="center"),
-                                    hovermode='x',   #hervermode: x 複数参照、 closest　一番近い点
-                            legend=dict(xanchor='right',
-                                        yanchor='bottom',
-                                        x=1.25,
-                                        y=0.35,
-                                        orientation='v',
-                                        bgcolor="white",
-                                        bordercolor="grey",
-                                        borderwidth=1
-                                    )
-                                    )
+        fig1.update_layout(
+            title=dict(
+                text="<b>StockPrice_Chart",
+                font=dict(size=20, color="grey"),
+                x=0.45,
+                xanchor="center",
+            ),
+            hovermode="x",  # hervermode: x 複数参照、 closest　一番近い点
+            legend=dict(
+                xanchor="right",
+                yanchor="bottom",
+                x=1.25,
+                y=0.35,
+                orientation="v",
+                bgcolor="white",
+                bordercolor="grey",
+                borderwidth=1,
+            ),
+        )
     st.plotly_chart(fig1, use_container_width=True)
 
 
